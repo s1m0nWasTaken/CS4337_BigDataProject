a1190925a8d80b7c995343269e14b1be92c6b39e
Alex <21338787@studentmail.ul.ie>
2024-10-10 15:35:07 +0100

    add spotless code formatter and format code

--- src/main/java/CS4337/project/Project/ProjectApplication.java
+++ src/main/java/CS4337/project/Project/ProjectApplication.java
@@ -1,23 +1,21 @@
 package CS4337.Project.Project;
 
+import java.util.Map;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.Map;
-
 @SpringBootApplication
 @RestController
 public class ProjectApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(ProjectApplication.class, args);
-	}
+  public static void main(String[] args) {
+    SpringApplication.run(ProjectApplication.class, args);
+  }
 
-	@GetMapping("/")
-	public Map<String, Object> sayhello() {
-		return Map.of("message", "Hello World!");
-	}
-
+  @GetMapping("/")
+  public Map<String, Object> sayhello() {
+    return Map.of("message", "Hello World!");
+  }
 }
