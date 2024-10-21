d929130d0adcd5921594963df52f6a2acda816c3
Alex <21338787@studentmail.ul.ie>
2024-10-21 19:51:49 +0100

    spotlessapply for tests

--- UserService/src/test/java/CS4337/Project/UserServiceTests.java
+++ UserService/src/test/java/CS4337/Project/UserServiceTests.java
@@ -32,22 +32,22 @@ class UserServiceTests {
     jdbcTemplate.execute("DROP TABLE IF EXISTS `User`;");
     jdbcTemplate.execute(
         "CREATE TABLE IF NOT EXISTS `User` (\n"
-        + "    id INT AUTO_INCREMENT PRIMARY KEY,\n"
-        + "    userType ENUM('admin', 'shopowner', 'customer') NOT NULL,\n"
-        + "    username VARCHAR(255) NOT NULL,\n"
-        + "    email VARCHAR(255) NOT NULL,\n"
-        + "    address VARCHAR(255),\n"
-        + "    isHidden BOOLEAN DEFAULT FALSE);");
+            + "    id INT AUTO_INCREMENT PRIMARY KEY,\n"
+            + "    userType ENUM('admin', 'shopowner', 'customer') NOT NULL,\n"
+            + "    username VARCHAR(255) NOT NULL,\n"
+            + "    email VARCHAR(255) NOT NULL,\n"
+            + "    address VARCHAR(255),\n"
+            + "    isHidden BOOLEAN DEFAULT FALSE);");
 
     jdbcTemplate.execute(
         "INSERT INTO `User` (userType, username, email, address, isHidden) "
-        + "VALUES ('shopowner', 'test1', 'test1@email.com', '11,11St',false)");
+            + "VALUES ('shopowner', 'test1', 'test1@email.com', '11,11St',false)");
     jdbcTemplate.execute(
         "INSERT INTO `User` (userType, username, email, address, isHidden) "
-        + "VALUES ('customer', 'test2', 'test2@email.com', '22,22St',true)");
+            + "VALUES ('customer', 'test2', 'test2@email.com', '22,22St',true)");
     jdbcTemplate.execute(
         "INSERT INTO `User` (userType, username, email, address, isHidden) "
-        + "VALUES ('admin', 'test3', 'test3@email.com', '33,33St',false)");
+            + "VALUES ('admin', 'test3', 'test3@email.com', '33,33St',false)");
   }
 
   @Test
@@ -62,7 +62,7 @@ class UserServiceTests {
     assertEquals(HttpStatus.OK, response.getStatusCode());
     Map<String, Object> responseBody = response.getBody();
     assertTrue(responseBody.containsKey("success"));
-    List<User> users = (List<User>)responseBody.get("success");
+    List<User> users = (List<User>) responseBody.get("success");
     assertEquals(3, users.size());
 
     // get hidden users
@@ -72,7 +72,7 @@ class UserServiceTests {
     assertEquals(HttpStatus.OK, response.getStatusCode());
     responseBody = response.getBody();
     assertTrue(responseBody.containsKey("success"));
-    List<User> usersH = (List<User>)responseBody.get("success");
+    List<User> usersH = (List<User>) responseBody.get("success");
     assertEquals(1, usersH.size());
 
     // get shown users
@@ -82,7 +82,7 @@ class UserServiceTests {
     assertEquals(HttpStatus.OK, response.getStatusCode());
     responseBody = response.getBody();
     assertTrue(responseBody.containsKey("success"));
-    List<User> usersS = (List<User>)responseBody.get("success");
+    List<User> usersS = (List<User>) responseBody.get("success");
     assertEquals(2, usersS.size());
   }
 }
