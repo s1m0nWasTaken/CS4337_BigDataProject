cdaa219b1eb10e1cb8097a7ad3a0513da96e9151
Niomahony <93268576+Niomahony@users.noreply.github.com>
2024-10-25 12:33:49 +0100

    apply spotless

--- ShopService/src/main/java/CS4337.Project/Shop.java
+++ ShopService/src/main/java/CS4337.Project/Shop.java
@@ -1,91 +1,95 @@
 package CS4337.Project;
 
 enum ShopType {
-    CLOTHING,
-    ELECTRONICS,
-    FOOD,
-    BOOKS,
-    TOYS,
-    OTHER
+  CLOTHING,
+  ELECTRONICS,
+  FOOD,
+  BOOKS,
+  TOYS,
+  OTHER
 }
 
 public class Shop {
-    private int id;
-    private int shopOwnerid;
-    private String shopName;
-    private String description;
-    private String imageData;
-    private ShopType shopType;
-    private String shopEmail;
+  private int id;
+  private int shopOwnerid;
+  private String shopName;
+  private String description;
+  private String imageData;
+  private ShopType shopType;
+  private String shopEmail;
 
+  public Shop() {}
 
-    public Shop() {
-    }
+  public Shop(
+      int id,
+      int shopOwnerid,
+      String shopName,
+      String description,
+      String imageData,
+      ShopType shopType,
+      String shopEmail) {
+    this.id = id;
+    this.shopOwnerid = shopOwnerid;
+    this.shopName = shopName;
+    this.description = description;
+    this.imageData = imageData;
+    this.shopType = shopType;
+    this.shopEmail = shopEmail;
+  }
 
-    public Shop(int id, int shopOwnerid, String shopName, String description, String imageData, ShopType shopType, String shopEmail) {
-        this.id = id;
-        this.shopOwnerid = shopOwnerid;
-        this.shopName = shopName;
-        this.description = description;
-        this.imageData = imageData;
-        this.shopType = shopType;
-        this.shopEmail = shopEmail;
-    }
+  public int getId() {
+    return id;
+  }
 
-    public int getId() {
-        return id;
-    }
+  public void setId(int id) {
+    this.id = id;
+  }
 
-    public void setId(int id) {
-        this.id = id;
-    }
+  public int getShopOwnerid() {
+    return shopOwnerid;
+  }
 
-    public int getShopOwnerid() {
-        return shopOwnerid;
-    }
+  public void setShopOwnerid(int shopOwnerid) {
+    this.shopOwnerid = shopOwnerid;
+  }
 
-    public void setShopOwnerid(int shopOwnerid) {
-        this.shopOwnerid = shopOwnerid;
-    }
+  public String getShopName() {
+    return shopName;
+  }
 
-    public String getShopName() {
-        return shopName;
-    }
+  public void setShopName(String shopName) {
+    this.shopName = shopName;
+  }
 
-    public void setShopName(String shopName) {
-        this.shopName = shopName;
-    }
+  public String getDescription() {
+    return description;
+  }
 
-    public String getDescription() {
-        return description;
-    }
+  public void setDescription(String description) {
+    this.description = description;
+  }
 
-    public void setDescription(String description) {
-        this.description = description;
-    }
+  public String getImageData() {
+    return imageData;
+  }
 
-    public String getImageData() {
-        return imageData;
-    }
+  public void setImageData(String imageData) {
+    this.imageData = imageData;
+  }
 
-    public void setImageData(String imageData) {
-        this.imageData = imageData;
-    }
+  public ShopType getShopType() {
+    return shopType;
+  }
 
-    public ShopType getShopType() {
-        return shopType;
-    }
+  public void setShopType(ShopType shopType) {
+    this.shopType = shopType;
+  }
 
-    public void setShopType(ShopType shopType) {
-        this.shopType = shopType;
-    }
+  public String getShopEmail() {
+    return shopEmail;
+  }
 
-    public String getShopEmail() {
-        return shopEmail;
-    }
-
-    public void setShopEmail(String shopEmail) {
-        this.shopEmail = shopEmail;
-    }
-
+  public void setShopEmail(String shopEmail) {
+    this.shopEmail = shopEmail;
+  }
 }
--- ShopService/src/main/java/CS4337.Project/ShopService.java
+++ ShopService/src/main/java/CS4337.Project/ShopService.java
@@ -1,5 +1,7 @@
 package CS4337.Project;
 
+import java.util.List;
+import java.util.Map;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -9,190 +11,188 @@ import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.core.simple.SimpleJdbcInsert;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.List;
-import java.util.Map;
-
 @SpringBootApplication
 @RestController
 public class ShopService {
-    @Autowired
-    private JdbcTemplate jdbcTemplate;
+  @Autowired private JdbcTemplate jdbcTemplate;
 
-    public static void main(String[] args) {
-        SpringApplication.run(ShopService.class, args);
+  public static void main(String[] args) {
+    SpringApplication.run(ShopService.class, args);
+  }
+
+  @GetMapping("/shop")
+  public List<Map<String, Object>> shop() {
+    List<Map<String, Object>> shops;
+    try {
+      shops = jdbcTemplate.queryForList("SELECT * FROM Shop");
+    } catch (DataAccessException e) {
+      jdbcTemplate.execute("USE user_service;");
+      jdbcTemplate.execute(
+          "CREATE TABLE `ShopItem` ("
+              + "id INT AUTO_INCREMENT PRIMARY KEY, "
+              + "shopOwnerid INT NOT NULL, "
+              + "shopName VARCHAR(255) NOT NULL, "
+              + "imageData VARCHAR(255), "
+              + "description VARCHAR(255), "
+              + "shopType ENUM(CLOTHING, ELECTRONICS,FOOD, BOOKS, TOYS, OTHER) NOT NULL, "
+              + "shopEmail VARCHAR (255) NOT NULL);");
+      shops = jdbcTemplate.queryForList(" SELECT * FROM Shop ");
     }
+    return shops;
+  }
 
-    @GetMapping("/shop")
-    public List<Map<String, Object>> shop() {
-        List<Map<String, Object>> shops;
-        try {
-            shops = jdbcTemplate.queryForList("SELECT * FROM Shop");
-        } catch (DataAccessException e) {
-            jdbcTemplate.execute("USE user_service;");
-            jdbcTemplate.execute(
-                    "CREATE TABLE `ShopItem` ("
-                            + "id INT AUTO_INCREMENT PRIMARY KEY, "
-                            + "shopOwnerid INT NOT NULL, "
-                            + "shopName VARCHAR(255) NOT NULL, "
-                            + "imageData VARCHAR(255), "
-                            + "description VARCHAR(255), "
-                            + "shopType ENUM(CLOTHING, ELECTRONICS,FOOD, BOOKS, TOYS, OTHER) NOT NULL, "
-                            + "shopEmail VARCHAR (255) NOT NULL);");
-            shops = jdbcTemplate.queryForList(" SELECT * FROM Shop ");
-        }
-        return shops;
+  @PostMapping("/shop")
+  public Map<String, Object> addShop(@RequestBody Shop shop) {
+    // needs user access checking
+    try {
+      SimpleJdbcInsert inserter = new SimpleJdbcInsert(jdbcTemplate);
+      String sqlInsert =
+          "INSERT INTO 'Shop' (shopOwnerid, shopName, imageData, description, shopType, shopEmail) "
+              + "VALUES (?, ?, ?, ?, ?, ?)";
+      jdbcTemplate.update(
+          sqlInsert,
+          shop.getShopOwnerid(),
+          shop.getShopName(),
+          shop.getImageData(),
+          shop.getDescription(),
+          shop.getShopType().name(),
+          shop.getShopEmail());
+      return Map.of("success", 1);
+    } catch (TransientDataAccessResourceException e) {
+      return Map.of("error", e.getMessage());
     }
+  }
 
-    @PostMapping("/shop")
-    public Map<String, Object> addShop(@RequestBody Shop shop) {
-        //needs user access checking
-        try {
-            SimpleJdbcInsert inserter = new SimpleJdbcInsert(jdbcTemplate);
-            String sqlInsert =
-                    "INSERT INTO 'Shop' (shopOwnerid, shopName, imageData, description, shopType, shopEmail) " + "VALUES (?, ?, ?, ?, ?, ?)";
-            jdbcTemplate.update(
-                    sqlInsert,
-                    shop.getShopOwnerid(),
-                    shop.getShopName(),
-                    shop.getImageData(),
-                    shop.getDescription(),
-                    shop.getShopType().name(),
-                    shop.getShopEmail());
-            return Map.of("success", 1);
-        } catch (TransientDataAccessResourceException e) {
-            return Map.of("error", e.getMessage());
-        }
+  @GetMapping("/shopitem")
+  public List<Map<String, Object>> shopItem() {
+    List<Map<String, Object>> shopItems;
+    try {
+      shopItems = jdbcTemplate.queryForList("SELECT * FROM ShopItem");
+    } catch (DataAccessException e) {
+      jdbcTemplate.execute("USE user_service;");
+      jdbcTemplate.execute(
+          "CREATE TABLE `ShopItem` ("
+              + "id INT AUTO_INCREMENT PRIMARY KEY, "
+              + "shopid INT NOT NULL, "
+              + "price DOUBLE(10, 2), "
+              + "stock INT NOT NULL"
+              + "picture VARCHAR(255), "
+              + "description VARCHAR(255), "
+              + "canUpdate BOOL DEFAULT TRUE, "
+              + "isHidden BOOL DEFAULT TRUE);");
+      shopItems = jdbcTemplate.queryForList(" SELECT * FROM ShopItem ");
     }
+    return shopItems;
+  }
 
-    @GetMapping("/shopitem")
-    public List<Map<String, Object>> shopItem() {
-        List<Map<String, Object>> shopItems;
-        try {
-            shopItems = jdbcTemplate.queryForList("SELECT * FROM ShopItem");
-        } catch (DataAccessException e) {
-            jdbcTemplate.execute("USE user_service;");
-            jdbcTemplate.execute(
-                    "CREATE TABLE `ShopItem` ("
-                            + "id INT AUTO_INCREMENT PRIMARY KEY, "
-                            + "shopid INT NOT NULL, "
-                            + "price DOUBLE(10, 2), "
-                            + "stock INT NOT NULL"
-                            + "picture VARCHAR(255), "
-                            + "description VARCHAR(255), "
-                            + "canUpdate BOOL DEFAULT TRUE, "
-                            + "isHidden BOOL DEFAULT TRUE);");
-            shopItems = jdbcTemplate.queryForList(" SELECT * FROM ShopItem ");
-        }
-        return shopItems;
+  @PostMapping("/shopItem")
+  public Map<String, Object> addShopItem(@RequestBody ShopItem shopItem) {
+    // need to add user access checking
+    try {
+      SimpleJdbcInsert inserter = new SimpleJdbcInsert(jdbcTemplate);
+      String sqlInsert =
+          "INSERT INTO 'ShopItem' (shopid, price, stock, picture, description) "
+              + "VALUES (?, ?, ?, ?, ?)";
+      jdbcTemplate.update(
+          sqlInsert,
+          shopItem.getShopid(),
+          shopItem.getPrice(),
+          shopItem.getStock(),
+          shopItem.getPicture(),
+          shopItem.getDescription());
+      return Map.of("success", 1);
+    } catch (TransientDataAccessResourceException e) {
+      return Map.of("error", e.getMessage());
     }
+  }
 
-    @PostMapping("/shopItem")
-    public Map<String, Object> addShopItem(@RequestBody ShopItem shopItem) {
-        //need to add user access checking
-        try {
-            SimpleJdbcInsert inserter = new SimpleJdbcInsert(jdbcTemplate);
-            String sqlInsert =
-                    "INSERT INTO 'ShopItem' (shopid, price, stock, picture, description) " + "VALUES (?, ?, ?, ?, ?)";
-            jdbcTemplate.update(
-                    sqlInsert,
-                    shopItem.getShopid(),
-                    shopItem.getPrice(),
-                    shopItem.getStock(),
-                    shopItem.getPicture(),
-                    shopItem.getDescription());
-            return Map.of("success", 1);
-        } catch (TransientDataAccessResourceException e) {
-            return Map.of("error", e.getMessage());
-        }
+  @PutMapping("/shopItem/{id}")
+  public Map<String, Object> updateShopItem(@PathVariable int id, @RequestBody ShopItem shopItem) {
+    // need to add user access checking
+
+    String updateQuery =
+        "UPDATE ShopItem SET price = ?, stock = ?, picture = ?, description = ? WHERE id = ?";
+
+    try {
+      int rowsAffected =
+          jdbcTemplate.update(
+              updateQuery,
+              shopItem.getShopid(),
+              shopItem.getPrice(),
+              shopItem.getStock(),
+              shopItem.getPicture(),
+              shopItem.getDescription(),
+              id);
+
+      if (rowsAffected > 0) {
+        return Map.of("success", 1, "message", "Shop item updated successfully");
+      } else {
+        return Map.of("success", 0, "message", "Shop item not found");
+      }
+    } catch (DataAccessException e) {
+      return Map.of("error", e.getMessage());
     }
+  }
 
-    @PutMapping("/shopItem/{id}")
-    public Map<String, Object> updateShopItem(@PathVariable int id, @RequestBody ShopItem shopItem) {
-        //need to add user access checking
+  @PutMapping("/shop/{id}")
+  public Map<String, Object> updateShop(@PathVariable int id, @RequestBody Shop shop) {
+    // add checking for only shopOwnerId allocated to shop allowed to update shop
+    String updateQuery =
+        "UPDATE Shop SET shopOwnerid = ?, shopName = ?, imageData = ?, description = ?, shopType = ?, shopEmail = ? WHERE id = ?";
 
-        String updateQuery = "UPDATE ShopItem SET price = ?, stock = ?, picture = ?, description = ? WHERE id = ?";
+    try {
+      int rowsAffected =
+          jdbcTemplate.update(
+              updateQuery,
+              shop.getShopOwnerid(),
+              shop.getShopName(),
+              shop.getImageData(),
+              shop.getDescription(),
+              shop.getShopType().name(),
+              shop.getShopEmail(),
+              id);
 
-        try {
-            int rowsAffected = jdbcTemplate.update(
-                    updateQuery,
-                    shopItem.getShopid(),
-                    shopItem.getPrice(),
-                    shopItem.getStock(),
-                    shopItem.getPicture(),
-                    shopItem.getDescription(),
-                    id
-            );
-
-            if (rowsAffected > 0) {
-                return Map.of("success", 1, "message", "Shop item updated successfully");
-            } else {
-                return Map.of("success", 0, "message", "Shop item not found");
-            }
-        } catch (DataAccessException e) {
-            return Map.of("error", e.getMessage());
-        }
+      if (rowsAffected > 0) {
+        return Map.of("success", 1, "message", "Shop updated successfully");
+      } else {
+        return Map.of("success", 0, "message", "Shop not found");
+      }
+    } catch (DataAccessException e) {
+      return Map.of("error", e.getMessage());
     }
+  }
 
-    @PutMapping("/shop/{id}")
-    public Map<String, Object> updateShop(@PathVariable int id, @RequestBody Shop shop) {
-        //add checking for only shopOwnerId allocated to shop allowed to update shop
-        String updateQuery = "UPDATE Shop SET shopOwnerid = ?, shopName = ?, imageData = ?, description = ?, shopType = ?, shopEmail = ? WHERE id = ?";
+  @DeleteMapping("/shop/{id}")
+  public Map<String, Object> deleteShop(@PathVariable int id) {
+    String deleteQuery = "DELETE FROM Shop WHERE id = ?";
 
-        try {
-            int rowsAffected = jdbcTemplate.update(
-                    updateQuery,
-                    shop.getShopOwnerid(),
-                    shop.getShopName(),
-                    shop.getImageData(),
-                    shop.getDescription(),
-                    shop.getShopType().name(),
-                    shop.getShopEmail(),
-                    id
-            );
+    try {
+      int rowsAffected = jdbcTemplate.update(deleteQuery, id);
 
-            if (rowsAffected > 0) {
-                return Map.of("success", 1, "message", "Shop updated successfully");
-            } else {
-                return Map.of("success", 0, "message", "Shop not found");
-            }
-        } catch (DataAccessException e) {
-            return Map.of("error", e.getMessage());
-        }
+      if (rowsAffected > 0) {
+        return Map.of("success", 1, "message", "Shop deleted successfully");
+      } else {
+        return Map.of("success", 0, "message", "Shop not found");
+      }
+    } catch (DataAccessException e) {
+      return Map.of("error", e.getMessage());
     }
+  }
 
-    @DeleteMapping("/shop/{id}")
-    public Map<String, Object> deleteShop(@PathVariable int id) {
-        String deleteQuery = "DELETE FROM Shop WHERE id = ?";
+  @DeleteMapping("/shopItem/{id}")
+  public Map<String, Object> deleteShopItem(@PathVariable int id) {
+    String deleteQuery = "DELETE FROM ShopItem WHERE id = ?";
 
-        try {
-            int rowsAffected = jdbcTemplate.update(deleteQuery, id);
+    try {
+      int rowsAffected = jdbcTemplate.update(deleteQuery, id);
 
-            if (rowsAffected > 0) {
-                return Map.of("success", 1, "message", "Shop deleted successfully");
-            } else {
-                return Map.of("success", 0, "message", "Shop not found");
-            }
-        } catch (DataAccessException e) {
-            return Map.of("error", e.getMessage());
-        }
+      if (rowsAffected > 0) {
+        return Map.of("success", 1, "message", "Shop item deleted successfully");
+      } else {
+        return Map.of("success", 0, "message", "Shop item not found");
+      }
+    } catch (DataAccessException e) {
+      return Map.of("error", e.getMessage());
     }
-
-    @DeleteMapping("/shopItem/{id}")
-    public Map<String, Object> deleteShopItem(@PathVariable int id) {
-        String deleteQuery = "DELETE FROM ShopItem WHERE id = ?";
-
-        try {
-            int rowsAffected = jdbcTemplate.update(deleteQuery, id);
-
-            if (rowsAffected > 0) {
-                return Map.of("success", 1, "message", "Shop item deleted successfully");
-            } else {
-                return Map.of("success", 0, "message", "Shop item not found");
-            }
-        } catch (DataAccessException e) {
-            return Map.of("error", e.getMessage());
-        }
-    }
-
-
+  }
 }
