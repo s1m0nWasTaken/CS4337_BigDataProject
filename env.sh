31cdae42cab425b39dd9b6c892d8fda23dc9eb4a
Alex <21338787@studentmail.ul.ie>
2024-10-29 13:08:21 +0000

    explain network in env.sh

--- env.sh
+++ env.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 TARGET_DIRS=("UserService" "ShopService" "MessagingService" "BanService")
 
+#NB!! remember to make the external network(nessesary to let container http eachother):
+#`docker network create netw`
     #export NETWORK_NAME=netw
     #user service
     #export USER_HOST_PORT=9090
@@ -27,5 +29,11 @@ for TARGET_DIR in "${TARGET_DIRS[@]}"; do
         echo "SHOP_CONTAINER=shop-service"
         echo "SHOP_MYSQL_HOST_PORT=3308"
         echo "SHOP_MYSQL_CONTAINER_PORT=3306"
+        echo "#messaging service"
+        echo "MSG_HOST_PORT=8081"
+        echo "MSG_CONTAINER_PORT=8081"
+        echo "MSG_CONTAINER=messaging-service"
+        echo "MSG_MYSQL_HOST_PORT=3308"
+        echo "MSG_MYSQL_CONTAINER_PORT=3306"
     } > "$SCRIPT_DIR/$TARGET_DIR/.env"
 done
