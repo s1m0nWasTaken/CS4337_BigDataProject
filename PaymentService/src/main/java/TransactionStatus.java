34e7f54072934265a8a31c70b21a2620d6956cc3
Niomahony <93268576+Niomahony@users.noreply.github.com>
2024-11-12 11:00:39 +0000

    change enum haha oops

--- PaymentService/src/main/java/TransactionStatus.java
+++ PaymentService/src/main/java/TransactionStatus.java
@@ -1,6 +1,7 @@
 public enum TransactionStatus {
-    PROCESSING,
-    TRANSIT,
-    OUTFORDELIVERY,
-    DELIVERED
+    PENDING,     // Transaction is created but not yet completed
+    COMPLETED,   // Transaction was completed successfully
+    FAILED,      // Transaction failed due to some error
+    REFUNDED,    // Transaction was refunded
+    CANCELED     // Transaction was canceled by the user or system
 }
