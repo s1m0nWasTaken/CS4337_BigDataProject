f37523441df284754c961b1a96ac855304db6c0f
Bayan <b.nezamabad@gmail.com>
2024-11-13 15:21:35 +0000

    [Bayan] Created API gateway microservice

---
+++ ApiGateway/src/main/java/CS4337/Project/ApiGatewayApplication.java
@@ -0,0 +1,15 @@
+package CS4337.Project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.gateway.filter.GlobalFilter;
+import org.springframework.context.annotation.Bean;
+
+@SpringBootApplication
+public class ApiGatewayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ApiGatewayApplication.class, args);
+    }
+}
+
