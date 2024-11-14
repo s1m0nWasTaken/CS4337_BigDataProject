3638d9ccbd81af75328ff130a033e8bdc291b55f
Simon <21323208@studentmail.ul.ie>
2024-11-14 18:45:58 +0000

    [Szymon] Updated Rating to have delete&update.

--- RatingService/src/main/java/CS4337/Project/RatingService/RatingController.java
+++ RatingService/src/main/java/CS4337/Project/RatingService/RatingController.java
@@ -27,6 +27,10 @@ public class RatingController {
       if (shopid == null || userid == null || message == null || rating == null) {
         return ResponseEntity.badRequest().body(Map.of("error", "All parameters are required"));
       }
+      int amountOfRating = ratingRepository.checkRating(shopid, userid).size();
+      if (amountOfRating > 0) {
+        return ResponseEntity.badRequest().body(Map.of("error", "Rating already exists"));
+      }
 
       int result = ratingRepository.addRating(shopid, userid, message, rating);
       return ResponseEntity.ok(
@@ -36,6 +40,36 @@ public class RatingController {
     } catch (Exception e) {
       return ResponseEntity.status(500).body(Map.of("error", e.getMessage()));
     }
+  }
+
+  @PutMapping("/update/{id}")
+  public ResponseEntity<Map<String, String>> updateRating(
+      @PathVariable int id, @RequestBody Map<String, Object> payload) {
+    try {
+      String message = (String) payload.get("message");
+      Integer rating = (Integer) payload.get("rating");
+
+      if (message == null || rating == null) {
+        return ResponseEntity.badRequest().body(Map.of("error", "All parameters are required"));
+      }
+
+      int result = ratingRepository.updateRating(id, message, rating);
+      return ResponseEntity.ok(
+          Map.of(
+              "result",
+              result > 0 ? "Rating updated successfully" : "Rating not updated successfully"));
+    } catch (Exception e) {
+      return ResponseEntity.status(500).body(Map.of("error", e.getMessage()));
+    }
+  }
+
+  @DeleteMapping("/delete/{id}")
+  public ResponseEntity<Map<String, String>> deleteRating(@PathVariable int id) {
+    int result = ratingRepository.deleteRating(id);
+    return ResponseEntity.ok(
+        Map.of(
+            "result",
+            result > 0 ? "Rating deleted successfully" : "Rating not deleted successfully"));
   }
 
   @GetMapping("/shop/{shopid}")
