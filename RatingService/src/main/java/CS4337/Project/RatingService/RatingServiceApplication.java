53cd0c4e88d9045504e632fc1f2b892294f5e211
Simon <21323208@studentmail.ul.ie>
2024-11-14 17:15:46 +0000

    [Szymon] Working state
    Signed-off-by: Simon <21323208@studentmail.ul.ie>

--- RatingService/src/main/java/CS4337/Project/RatingService/RatingServiceApplication.java
+++ RatingService/src/main/java/CS4337/Project/RatingService/RatingServiceApplication.java
@@ -2,9 +2,7 @@ package CS4337.Project.RatingService;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
-@EnableDiscoveryClient
 @SpringBootApplication
 public class RatingServiceApplication {
 
