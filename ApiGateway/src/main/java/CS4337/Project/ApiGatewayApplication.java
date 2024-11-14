950c45db9cf80d1bc6a4d200e94d3338b21d87c4
Bayan <b.nezamabad@gmail.com>
2024-11-14 16:38:05 +0000

    [Bayan] Added env vars, and Alex's routes

--- ApiGateway/src/main/java/CS4337/Project/ApiGatewayApplication.java
+++ ApiGateway/src/main/java/CS4337/Project/ApiGatewayApplication.java
@@ -2,8 +2,6 @@ package CS4337.Project;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.gateway.filter.GlobalFilter;
-import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class ApiGatewayApplication {
