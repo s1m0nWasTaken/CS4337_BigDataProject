00434c8a721eafb738fda77e70ee7c1e766a86b9
Bayan <b.nezamabad@gmail.com>
2024-12-06 15:50:32 +0000

    [Bayan] Spotless apply

--- RatingService/src/test/java/CS4337/Project/RatingControllerTest.java
+++ RatingService/src/test/java/CS4337/Project/RatingControllerTest.java
@@ -11,7 +11,6 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
-
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
